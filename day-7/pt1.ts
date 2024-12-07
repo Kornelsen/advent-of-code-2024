8ae7ffd977a7ac8458a17e3caf8c4baf8b0b795a
Kornelsen <kornelsen>
2024-12-07 13:57:26 -0500

    day 7 solution

---
+++ day-7/pt1.ts
@@ -0,0 +1,31 @@
+const input = await Deno.readTextFile("./sample.txt");
+const lines = input.split("\n");
+
+let result = 0;
+
+for (const line of lines) {
+  const puzzle = line.split(": ");
+  const target = +puzzle[0];
+  const values = puzzle[1].split(" ");
+
+  let valid = false;
+
+  const backtrack = (curr: number, target: number, index: number) => {
+    if (valid) return;
+    if (curr > target) return;
+
+    const value = +values[index];
+    if (!value) {
+      if (curr === target) valid = true;
+      return;
+    }
+
+    backtrack(curr * value, target, index + 1);
+    backtrack(curr + value, target, index + 1);
+  };
+
+  backtrack(+values[0], target, 1);
+  if (valid) result += target;
+}
+
+console.log(result);
